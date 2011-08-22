7b1a4da23f362fa00b9362487c6841ddf542ac11
Alexander Boyd <alex@opengroove.org>
2011-08-21 19:31:58 -0600

    Changed version number to 0.1.24 in preparation for a release

--- parcon/setup.py
+++ parcon/setup.py
@@ -4,7 +4,7 @@ from distutils.core import setup
 
 setup(
     name="parcon",
-    version="0.1.23",
+    version="0.1.24",
     author="Alexander Boyd",
     author_email="alex@opengroove.org",
     url="http://www.opengroove.org/parcon/",
